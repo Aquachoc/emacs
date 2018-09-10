b3db0bde7688fdd73f57b36dcded9d331eae5db5
Aquachoc <aquachoc97@gmail.com>
2018-09-10 23:42:01 +0200

    First commit

---
+++ elpa/popup-20160709.1429/popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("popup.el") (23446 43359 849426 258000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
